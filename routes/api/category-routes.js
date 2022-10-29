d961da56ad98bbb1c860003e91d17d5b5362a925
Tobias Virgona <tobiasvirgona@gmail.com>
2022-10-29 12:03:49 +1100

    all routings complete. Execpt Product Create. Looks like it was already done? confused by this one

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -48,7 +48,14 @@ router.post('/', async (req, res) => {
 
 router.put('/:id', async (req, res) => {
   // update a category by its `id` value
-  const updateCategory = await Category
+  try {
+    const updateCategory = await Category.update(
+      req.body, { where: { id: req.params.id } }
+    );
+    res.status(200).json(updateCategory);
+  } catch (err) {
+    res.status(400).json(err);
+  }
 });
 
 router.delete('/:id', async (req, res) => {
